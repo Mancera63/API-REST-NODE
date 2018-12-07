8ce2ab2bf13b331ea92fe335cf4ab36eb4e8d7c1
Meli7 <14030642@itcelaya.edu.mx>
2018-12-06 19:41:57 -0600

    Cómo almacenar datos en MongoDB con HTTP POST en tu API | Curso NodeJS y MongoDB #0

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require ('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
+const Product = require('./models/product')
+
 const app = express()
 const port = process.env.PORT || 3001
 
@@ -13,14 +15,31 @@ app.use(bodyParser.json())
 app.get('/api/product', (req,res) => {  
  res.send(200,{products: []})
 })
+
 app.get('/api/product/:productId', (req,res) => {  
 
 })
+
 app.post('/api/product', (req,res) => {  
+console.log('POST /api/product')
 console.log(req.body)
-res.send(200, {message: 'EL PRODUCTO SE HA RECIBIDO'})
+
+let product = new Product()
+product.name = req.body.name
+product.picture = req.body.picture
+product.price = req.body.price
+product.category = req.body.category
+product.description = req.body.description
+
+product.save((err, productStored)=>{
+    if(err) res.status(500).send({message: `Error al salvar la base de datos ${err}`})
+
+    res.status(200).send({product: productStored})
 })
-app.get('/api/product/:productId', (req,res) => {  
+
+})
+
+app.put('/api/product/:productId', (req,res) => {  
 
 })
 
